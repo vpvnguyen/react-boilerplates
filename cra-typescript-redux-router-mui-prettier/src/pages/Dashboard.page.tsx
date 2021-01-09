e3706e6ef193156a7f2833d6e78f8b41877b82fd
vpvnguyen <vpvnguyen@gmail.com>
2021-01-09 14:52:45 -0800

    create example dashboard page

---
+++ cra-typescript-redux-router-mui-prettier/src/pages/Dashboard.page.tsx
@@ -0,0 +1,10 @@
+import DashboardComponent from "../components/Dashboard.component";
+
+const DashboardPage = () => (
+  <div>
+    <h1>Dashboard Page</h1>
+    <DashboardComponent />
+  </div>
+);
+
+export default DashboardPage;
