71c971f437cb318749ee1443685c974b98c4bf0b
vpvnguyen <vpvnguyen@gmail.com>
2021-01-09 14:07:09 -0800

    render redux provider

--- cra-typescript-redux-router-mui-prettier/src/index.tsx
+++ cra-typescript-redux-router-mui-prettier/src/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
-import App from "./App";
+import store from "./redux/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const render = () => {
+  const App = require("./App").default;
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === "development" && module.hot)
+  module.hot.accept("./App", render);
