165e84972eaa64e49d07afb093d1ba2f8a249989
vpvnguyen <vpvnguyen@gmail.com>
2021-01-08 23:06:47 -0800

    display NODE_ENV app environment

--- cra-typescript-redux-router-mui-prettier/src/App.tsx
+++ cra-typescript-redux-router-mui-prettier/src/App.tsx
@@ -6,17 +6,8 @@ const App = () => (
     <header className='App-header'>
       <img src={logo} className='App-logo' alt='logo' />
       <p>
-        Edit <code>src/App.tsx</code> and save to reload.
+        Environment: <code>{process.env.NODE_ENV}</code>
       </p>
-      {process.env.NODE_ENV}
-      <a
-        className='App-link'
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Learn React
-      </a>
     </header>
   </div>
 );
