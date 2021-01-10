29eaa419e46bf09e0da92dcdbddd8ba100500daa
vpvnguyen <vpvnguyen@gmail.com>
2021-01-10 13:18:39 -0800

    fix store import module; removed redux dev hot module

--- cra-typescript-redux-router-mui-prettier/src/index.tsx
+++ cra-typescript-redux-router-mui-prettier/src/index.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import App from "./App";
 import "./index.css";
-import store from "./redux/store";
+import { store } from "./redux/store";
 
-const render = () => {
-  const App = require("./App").default;
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-render();
-
-if (process.env.NODE_ENV === "development" && module.hot)
-  module.hot.accept("./App", render);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
