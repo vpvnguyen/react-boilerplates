912869357e4821a1668defaabf9e800e206a89fd
vpvnguyen <vpvnguyen@gmail.com>
2021-01-10 13:38:35 -0800

    add border box around component

--- cra-typescript-redux-router-mui-prettier/src/components/Dashboard.component.tsx
+++ cra-typescript-redux-router-mui-prettier/src/components/Dashboard.component.tsx
@@ -1,10 +1,15 @@
+import { CSSProperties } from "react";
 import { Typography } from "@material-ui/core";
 
+const style: CSSProperties = {
+  borderStyle: "solid",
+};
+
 const DashboardComponent = () => (
-  <>
+  <div style={style}>
     <Typography>Dashboard component</Typography>
     <Typography>You are not logged in</Typography>
-  </>
+  </div>
 );
 
 export default DashboardComponent;
--- cra-typescript-redux-router-mui-prettier/src/components/Home.component.tsx
+++ cra-typescript-redux-router-mui-prettier/src/components/Home.component.tsx
@@ -1,10 +1,15 @@
+import { CSSProperties } from "react";
 import { Typography } from "@material-ui/core";
 
+const style: CSSProperties = {
+  borderStyle: "solid",
+};
+
 const HomeComponent = () => (
-  <>
+  <div style={style}>
     <Typography>Home component</Typography>
     <Typography>Welcome!</Typography>
-  </>
+  </div>
 );
 
 export default HomeComponent;
