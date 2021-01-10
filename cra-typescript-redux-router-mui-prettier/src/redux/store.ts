fb5cc74f98dc3e92c61320c367d04e6cf4424b32
vpvnguyen <vpvnguyen@gmail.com>
2021-01-10 13:19:04 -0800

    refactor store to include combined reducers

--- cra-typescript-redux-router-mui-prettier/src/redux/store.ts
+++ cra-typescript-redux-router-mui-prettier/src/redux/store.ts
@@ -1,17 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./rootReducer";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "./reducers/counterSlice";
 
-const store = configureStore({
-  reducer: rootReducer,
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
 });
 
-if (process.env.NODE_ENV === "development" && module.hot) {
-  module.hot.accept("./rootReducer", () => {
-    const newRootReducer = require("./rootReducer").defaultstore;
-    store.replaceReducer(newRootReducer);
-  });
-}
-
-export type AppDispatch = typeof store.dispatch;
-
-export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
