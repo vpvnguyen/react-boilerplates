3b8c303529cbe3faae6a6ba472079c568d981438
vpvnguyen <vpvnguyen@gmail.com>
2021-01-11 22:29:15 -0800

    export isAuthenticated boolean state

--- cra-typescript-redux-router-mui-prettier/src/redux/selectors/authenticationSelector.ts
+++ cra-typescript-redux-router-mui-prettier/src/redux/selectors/authenticationSelector.ts
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
 import { RootState } from "../store";
 
 export const selectAuthentication = (state: RootState) =>
   state.authentication.value;
+
+export const selectIsAuthenticated = (state: RootState) =>
+  state.authentication.isAuthenticated;
