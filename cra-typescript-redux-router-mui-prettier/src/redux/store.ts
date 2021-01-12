a786686f15da5dc35cfef05a88201bf2379e153a
vpvnguyen <vpvnguyen@gmail.com>
2021-01-11 18:24:16 -0800

    import authentication reducer

--- cra-typescript-redux-router-mui-prettier/src/redux/store.ts
+++ cra-typescript-redux-router-mui-prettier/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "./reducers/counterSlice";
+import authenticationReducer from "./reducers/authenticationSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    authentication: authenticationReducer,
   },
 });
 
