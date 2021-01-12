930b4e4020902ede7f68d6b166c0a86f2f9f1640
vpvnguyen <vpvnguyen@gmail.com>
2021-01-11 18:23:52 -0800

    use authentication state and dispatch ffrom redux

--- cra-typescript-redux-router-mui-prettier/src/components/Authentication.component.tsx
+++ cra-typescript-redux-router-mui-prettier/src/components/Authentication.component.tsx
@@ -1,21 +1,42 @@
 import { CSSProperties } from "react";
 import { Button, Typography } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { selectAuthentication } from "../redux/selectors/authenticationSelector";
+import {
+  authenticateAsync,
+  unauthenticateAsync,
+} from "../redux/reducers/authenticationSlice";
 
 const style: CSSProperties = {
   borderStyle: "solid",
 };
 
-const AuthenticationComponent = () => (
-  <div style={style}>
-    <Typography variant='h1'>Authentication Component</Typography>
-    <Typography>Authentication State: Logged out</Typography>
-    <Button variant='contained' size='large' color='primary'>
-      Login
-    </Button>
-    <Button variant='contained' size='large' color='secondary'>
-      Logout
-    </Button>
-  </div>
-);
+const AuthenticationComponent = () => {
+  const authentication = useSelector(selectAuthentication);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={style}>
+      <Typography variant='h1'>Authentication Component</Typography>
+      <Typography>Authentication State: {authentication}</Typography>
+      <Button
+        variant='contained'
+        size='large'
+        color='primary'
+        onClick={() => dispatch(authenticateAsync())}
+      >
+        Login
+      </Button>
+      <Button
+        variant='contained'
+        size='large'
+        color='secondary'
+        onClick={() => dispatch(unauthenticateAsync())}
+      >
+        Logout
+      </Button>
+    </div>
+  );
+};
 
 export default AuthenticationComponent;
